3eb86eb3b6ab7f8cee495ce90a984e3eec1fabcf
Christopher Rescorla <cgrescorla@gmail.com>
2016-09-27 08:48:45 -0600

    improved Othermir page using JS

--- js/modal.js
+++ js/modal.js
@@ -3,21 +3,20 @@
  */
 var modal = document.getElementById("myNewModal");
 var modalImg = document.getElementById("img01");
+var body = document.body;
 
 function myFunction(obj)
 {
     modal.style.display = "block";
     modalImg.src = obj.src;
+    body.style.overflow = "hidden";
 }
 
 window.onclick = function(event)
 {
-    if (event.target == modal)
+    if (event.target == modal || event.target == modalImg)
     {
         modal.style.display = "none";
-    }
-    else if (event.target == modalImg)
-    {
-        modal.style.display = "none";
+        body.style.overflow = "auto";
     }
 }
